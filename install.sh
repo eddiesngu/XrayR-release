97d24c9a4d45faf21beb82df880ce6cd0b59b1a4
eddiesngu <101732005+eddiesngu@users.noreply.github.com>
2022-03-28 21:52:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -100,100 +100,6 @@ install_acme() {
     curl https://get.acme.sh | sh
 }
 
-auto()
-{
-  echo -e "  Nhập tên web ( Mặc định là 2lands.me ): "
-  read -p "  Web tên:" api_host
-
-  [ -z "${api_host}" ]
-
-  echo "--------------------------------"
-  echo "  Bạn đã  ${api_host}"
-  echo "--------------------------------"
-
- read -p "  API KEY của web (api_key):" apikey
-  [ -z "${apikey}" ]
-  echo "-------------------------------"
-  echo "API KEY Web của bạn:" apikey
-  echo "-------------------------------"
-
-
-  read -p "  ID nút (Node_ID):" node_id
-  [ -z "${node_id}" ] && node_id=0
-  echo "-------------------------------"
-  echo -e "  Node_ID: ${node_id}"
-  echo "-------------------------------"
-
-  # giới hạn thiết bị
- read -p "  Giới hạn thiết bị :" DeviceLimit
-  [ -z "${DeviceLimit}" ] && DeviceLimit="3"
-  echo "-------------------------------"
-  echo "  Thiết bị tối đa là: ${DeviceLimit}"
-  echo "-------------------------------"
-}
-EOF
-  cat >config.yml <<EOF
-Log:
-  Level: none # Log level: none, error, warning, info, debug 
-  AccessPath: # /etc/XrayR/access.Log
-  ErrorPath: # /etc/XrayR/error.log
-DnsConfigPath: # /etc/XrayR/dns.json Path to dns config, check https://xtls.github.io/config/base/dns/ for help
-RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/base/route/ for help
-OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/base/outbound/ for help
-ConnetionConfig:
-  Handshake: 4 # Handshake time limit, Second
-  ConnIdle: 10 # Connection idle time limit, Second
-  UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
-  DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
-  BufferSize: 64 # The internal cache size of each connection, kB 
-Nodes:
-  -
-    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
-    ApiConfig:
-      ApiHost: "http://4g.quoctai.xyz/"
-      ApiKey: "daudau432000xxxxxxxx"
-      NodeID: 41
-      NodeType: V2ray # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
-      Timeout: 30 # Timeout for the api request
-      EnableVless: false # Enable Vless for V2ray Type
-      EnableXTLS: false # Enable XTLS for V2ray and Trojan
-      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
-      DeviceLimit: 0 # Local settings will replace remote settings, 0 means disable
-      RuleListPath: # /etc/XrayR/rulelist Path to local rulelist file
-    ControllerConfig:
-      ListenIP: 0.0.0.0 # IP address you want to listen
-      SendIP: 0.0.0.0 # IP address you want to send pacakage
-      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
-      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
-      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
-      DisableUploadTraffic: false # Disable Upload Traffic to the panel
-      DisableGetRule: false # Disable Get Rule from the panel
-      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
-      DisableSniffing: true # Disable domain sniffing 
-      EnableProxyProtocol: false # Only works for WebSocket and TCP
-      EnableFallback: false # Only support for Trojan and Vless
-      FallBackConfigs:  # Support multiple fallbacks
-        -
-          SNI: # TLS SNI(Server Name Indication), Empty for any
-          Path: # HTTP PATH, Empty for any
-          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
-          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
-      CertConfig:
-        CertMode: dns # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "node1.test.com" # Domain to cert
-        CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
-        KeyFile: /etc/XrayR/cert/node1.test.com.key
-        Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
-        Email: test@me.com
-        DNSEnv: # DNS ENV option used by DNS provider
-          ALICLOUD_ACCESS_KEY: aaa
-          ALICLOUD_SECRET_KEY: bbb
-EOF
- sed -i "s|NodeID:.*|NodeID: ${node_id}|" ./config.yml
- sed -i "s|ApiHost:.*|ApiHost: \"${api_host}\"|" ./config.yml
- sed -i "s|DeviceLimit:.*|DeviceLimit: ${DeviceLimit}|" ./config.yml
- sed -i "s|ApiKey:.*|ApiKey: ${apikey}|" ./config.yml
-}
 
 install_XrayR() {
     if [[ -e /usr/local/XrayR/ ]]; then
@@ -294,4 +200,3 @@ echo -e "${green}开始安装${plain}"
 install_base
 install_acme
 install_XrayR $1
-auto
